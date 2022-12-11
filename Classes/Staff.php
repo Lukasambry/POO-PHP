8c355a439c6377714142db9c0ff264fbb8100334
Lukas Ambry <lambry@wedigital.garden>
2022-12-11 15:46:30 +0100

    feat/staffClass

---
+++ Classes/Staff.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Classes;
+
+require_once("MagicalWeapon.php");
+
+class Staff extends MagicalWeapon
+{
+    public function __construct(
+        public string $name = "Atiesh, Greatstaff of the Guardian",
+        public string $description = "Atiesh, Greatstaff of the Guardian is a powerful staff passed down through the line of the Guardians of Tirisfal up until and including Medivh, the Last Guardian",
+        public int $type = 0,
+        public int $damage = 24,
+      ) {}
+
+}
+
+
+
+
+?>
